04737d7af001f40a9ca6932e55115fa2c6841514
optimal-aces-7121 <gauravsingh947041@gmail.com>
2026-04-16 18:50:38 +0530

    Added eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: { ecmaVersion: 2020, globals: { window: true, document: true, navigator: true, fetch: true, setTimeout: true, clearTimeout: true, setInterval: true, clearInterval: true } },
+    plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'warn',
+    },
+  },
+]
